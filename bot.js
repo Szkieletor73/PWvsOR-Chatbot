e7e620cf495f80656bb4da205b69bd06bd0c729e
Szkieletor <szkieletor73@gmail.com>
2015-10-25 11:24:14 +0100

    /rules

--- bot.js
+++ bot.js
@@ -32,21 +32,18 @@ bot.on("message", function(message){
 			
 			//anti lolz
 			if(message.author.username === "Lolzrfunni"){
-				for(var i = commandsArr.length(), i > 0, i--) {
-					if(commandsArr[i] === "eejitry" || commandsArr[i] === "eejitry."){
-						bot.reply(message, "fackorff");
-						i = 0;
-					}
+				if(command === "eejitry" || command === "eejitry."){
+					bot.reply(message, "fackorff");
 				}
 			}
 			//version info
 			if(command === "\\author" || command === "\\creator" || command === "\\info"){
-				bot.sendMessage(message.author, "Bot based on discord.js. Bot creator: /u/szkieletor on reddit. Contact my creator if you've encountered any problems!");
+				bot.sendMessage(message.author, "Bot based on discord.js. Bot creator: /u/szkieletor on reddit.\nSource on GitHub: https://github.com/Szkieletor73/PWvsOR-Chatbot\nContact my creator if you've encountered any problems!");
 			}
 
 			//help
 			if(command === "\\help"){
-				bot.sendMessage(message.author, "All commands work in PMs and in channels:\n\\roll XdY - roll X Y-sided dice\n\\chromamap (alias: \\map) - get a link to interactive map\n\\info (aliases: \\creator, \\author) - get contact details of my creator.\n\\troops (alias: \\trooptypes) - get basic troop type flowchart\n\\chromabot (aliases: \\chromabotcommands, \\battlecommands, \\chromacommands) - gives you a list of chromabot commands as well as a direct link to chromabot's /user/page.\n\\links - gives you a list of useful chroma-related links.\nMore to come.");
+				bot.sendMessage(message.author, "All commands work in PMs and in channels:\n\\roll XdY - roll X Y-sided dice\n\\chromamap (alias: \\map) - get a link to interactive map\n\\info (aliases: \\creator, \\author) - get contact details of my creator.\n\\troops (alias: \\trooptypes) - get basic troop type flowchart\n\\chromabot (aliases: \\chromabotcommands, \\battlecommands, \\chromacommands) - gives you a list of chromabot commands as well as a direct link to chromabot's /user/page.\n\\links - gives you a list of useful chroma-related links.\n\\rules - gives you chat rules. They're also linked in \\links\nMore to come.");
 			}
 			
 			//simple testing message
@@ -113,7 +110,12 @@ bot.on("message", function(message){
 			
 			//links
 			if(command === "\\links"){
-				bot.sendMessage(message.author, "Chromabot's userpage: https://www.reddit.com/user/chromabot\nCouncil of Karma: https://www.reddit.com/r/councilofkarma\nPeriwinkle: https://www.reddit.com/r/periwinkle\nOrangered: https://www.reddit.com/r/orangered\nChromanauts: https://www.reddit.com/r/chromanauts\nField of Karmic Glory: https://www.reddit.com/r/FieldOfKarmicGlory\nChromalore: https://www.reddit.com/r/Chromalore\nInteractive map: http://periwinklevsorangered.com/map/index.php\nGMP Dubtrack: https://www.dubtrack.fm/join/goodmorningperiwinkle");
+				bot.sendMessage(message.author, "Chat rules: https://docs.google.com/document/d/1j7I3VLkRWft0oBvhuQGUx0rMu7PXmfTS8k1uPXdallI/edit\nChromabot's userpage: https://www.reddit.com/user/chromabot\nCouncil of Karma: https://www.reddit.com/r/councilofkarma\nPeriwinkle: https://www.reddit.com/r/periwinkle\nOrangered: https://www.reddit.com/r/orangered\nChromanauts: https://www.reddit.com/r/chromanauts\nField of Karmic Glory: https://www.reddit.com/r/FieldOfKarmicGlory\nChromalore: https://www.reddit.com/r/Chromalore\nInteractive map: http://periwinklevsorangered.com/map/index.php\nGMP Dubtrack: https://www.dubtrack.fm/join/goodmorningperiwinkle");
+			}
+			
+			//rules
+			if(command === "\\rules"){
+				bot.sendMessage(message.author, "Chat rules: https://docs.google.com/document/d/1j7I3VLkRWft0oBvhuQGUx0rMu7PXmfTS8k1uPXdallI/edit");
 			}
 			
 		// } //anti lolz ends here hopefully we never have to deploy it again
@@ -121,7 +123,7 @@ bot.on("message", function(message){
 
 //fired when new person enters the server
 bot.on("serverNewMember", function(user, server){
-	bot.sendMessage(server.defaultChannel, "Welcome to Periwinkle vs Orangered Discord Chat! I'm a resident bot. Type \"\\help\" to get a list of available commands.\nIf you have any questions, ask a moderator!");
+	bot.sendMessage(server.defaultChannel, "Welcome to Periwinkle vs Orangered Discord Chat! I'm a resident bot. Type \"\\help\" to get a list of available commands. Type \"\\rules\" to get current chat rules!\nIf you have any questions, ask a moderator!");
 })
 
 //fired on logout/login
