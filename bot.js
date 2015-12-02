fd3ae80085c16535f48c889736809ed279b62eac
Paweł Babiuch <szkieletor73@gmail.com>
2015-12-02 20:42:55 +0100

    fix for welcome message
    i also just realized the commit history is a straight line
    fuck branches I guess
    yolo

--- bot.js
+++ bot.js
@@ -260,9 +260,11 @@ bot.on("message", function(message){
 } )
 
 //fired when new person enters the server
-bot.on("serverNewMember", function(server, user){
+bot.on("serverNewMember", function(user, server){
 	//it will also send this thing for any other server I deploy it too, but I'm too lazy to actually check the server so fuck it, no one will notice
-	bot.sendMessage(user, "Welcome to Periwinkle vs Orangered Discord Chat, " + user + "! I'm a resident bot. Type \"\\help\" to get a list of available commands. Type \"\\rules\" to get current chat rules.\nType \"\\info\" to get my creator's contact details and a link to GitHub repo with my code.\nIf you have any questions, ask a moderator!");
+    if(server.name == "Periwinkle VS Orangered"){
+      bot.sendMessage(server, "Welcome to Periwinkle vs Orangered Discord Chat, " + user + "! I'm a resident bot. Type \"\\help\" to get a list of available commands. Type \"\\rules\" to get current chat rules.\nType \"\\info\" to get my creator's contact details and a link to GitHub repo with my code.\nIf you have any questions, ask a moderator!");
+    }
 })
 
 //fired on logout/login
